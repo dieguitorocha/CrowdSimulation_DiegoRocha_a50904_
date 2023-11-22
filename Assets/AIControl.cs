7fc91f81c40be2b74914428853a138001387531d
dieguitorocha <100223185+dieguitorocha@users.noreply.github.com>
2023-11-22 23:51:09 +0000

    Crowd simulation-Tutorial: 1.Moving as one- Completo

---
+++ Assets/AIControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControl : MonoBehaviour
+{
+    public GameObject goal;
+    UnityEngine.AI.NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
