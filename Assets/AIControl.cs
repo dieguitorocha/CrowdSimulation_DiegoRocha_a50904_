bcdfbce917a8884fa7beef0fc40bd31401853b45
dieguitorocha <100223185+dieguitorocha@users.noreply.github.com>
2023-11-23 00:33:12 +0000

    Tutorial:Creating a City Crowd Part 1-Completo

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AIControl : MonoBehaviour
-{
-    public GameObject goal;
-    UnityEngine.AI.NavMeshAgent agent;
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    NavMeshAgent agent;
+    Animator anim;
 
 
-    // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+
+        agent = GetComponent<NavMeshAgent>();
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if(agent.remainingDistance < 1)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+
+        }
     }
 }
